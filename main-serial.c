01189df58eac4065e796e37e5d382ed0be8c8822
Stefanie Guenther <guenther@mathcces.rwth-aachen.de>
2018-05-23 12:01:21 +0200

    CoDi Derivative for serial version.

--- main-serial.c
+++ main-serial.c
@@ -6,21 +6,23 @@
 int main()
 {
 
-    RealReverse *Ytrain;       /**< State variables: contains the training data from Lars*/
-    RealReverse *design;       /**< Design variables for the network */
-    RealReverse  objective;    /**< Objective function to be minimized */
-    double      *Ytarget;      /**< Contains the target values from Lars */
-    int         *batch;        /**< Contains indicees of the batch elements */
-    int          nexamples;    /**< Number of elements in the training data */
-    int          nbatch;       /**< Size of a batch */
-    int          nstate;       /**< dimension of the training data */
-    int          ndesign;      /**< dimension of the design variables */
-    int          ntimes;       /**< Number of layers / time steps */
-    int          nchannels;    /**< Number of channels of the netword (width) */
-    double       deltat;       /**< Time step size */
-    double       T;            /**< Final time */
-    double       theta0;       /**< Initial design value */
-    double       alpha;        /**< Regularization parameter */
+    RealReverse *Ytrain;      /**< State variables: contains the training data from Lars*/
+    RealReverse *design;      /**< Design variables for the network */
+    RealReverse  objective;   /**< Objective function to be minimized */
+    double      *Ytarget;     /**< Contains the target values from Lars */
+    int         *batch;       /**< Contains indicees of the batch elements */
+    int          nexamples;   /**< Number of elements in the training data */
+    int          nbatch;      /**< Size of a batch */
+    int          nstate;      /**< dimension of the training data */
+    int          ndesign;     /**< dimension of the design variables */
+    int          ntimes;      /**< Number of layers / time steps */
+    int          nchannels;   /**< Number of channels of the netword (width) */
+    double       deltat;      /**< Time step size */
+    double       T;           /**< Final time */
+    double       theta0;      /**< Initial design value */
+    double       alpha;       /**< Regularization parameter */
+    double      *gradient;    /**< Gradient of objective function wrt design */
+    double       gnorm;       /**< Norm of the gradient */
     
 
     /* Problem setup */
@@ -37,10 +39,11 @@ int main()
     ndesign = (nchannels * nchannels + 1 )* ntimes;
 
     /* Allocate memory */
-    Ytrain  = (RealReverse*) malloc(nstate*sizeof(RealReverse));
-    design  = (RealReverse*) malloc(ndesign*sizeof(RealReverse));
-    Ytarget = (double*) malloc(nstate*sizeof(double));
-    batch   = (int*) malloc(nbatch*sizeof(int));
+    Ytrain   = (RealReverse*) malloc(nstate*sizeof(RealReverse));
+    design   = (RealReverse*) malloc(ndesign*sizeof(RealReverse));
+    gradient = (double*) malloc(ndesign*sizeof(double));
+    Ytarget  = (double*) malloc(nstate*sizeof(double));
+    batch    = (int*) malloc(nbatch*sizeof(int));
 
 
     /* Read in data */
@@ -58,6 +61,15 @@ int main()
     {
         batch[ibatch] = ibatch;
     }
+
+    /* Set up CoDiPack */
+    RealReverse::TapeType& codiTape = RealReverse::getGlobalTape();
+    codiTape.setActive();
+    for (int idesign = 0; idesign < ndesign; idesign++)
+    {
+        codiTape.registerInput( design[idesign] );
+    }
+
 
     /* Time-loop */
     objective = 0.0;
@@ -78,15 +90,25 @@ int main()
 
     }
 
+    /* Get the derivative */
+    codiTape.setPassive();
+    objective.setGradient(1.0);
+    codiTape.evaluate();
+    for (int idesign = 0; idesign < ndesign; idesign++)
+    {
+        gradient[idesign] = design[idesign].getGradient(); 
+        gnorm += pow(gradient[idesign],2);
+    }
+    gnorm = sqrt(gnorm);
+
     /* output */
-    write_data ("Yout.dat", Ytrain, nstate);
+    printf("\n Loss:         %1.14e", getValue(objective));
+    printf("\n Gradientnorm: %1.14e", gnorm);
+    printf("\n\n");
 
-    // printf("Y %1.14e\n", Ytrain[99 * nchannels + 0]);
-    // printf("Y %1.14e\n", Ytrain[99 * nchannels + 1]);
-    // printf("Y %1.14e\n", Ytrain[99 * nchannels + 2]);
-    // printf("Y %1.14e\n", Ytrain[99 * nchannels + 3]);
-
-    printf("\n Loss: %1.14e\n\n", objective.getValue());
+    /* Write data to file */
+    write_data("Yout.dat", Ytrain, nstate);
+    write_data("gradient.dat", gradient, ndesign);
 
     /* free memory */
     free(Ytrain);
